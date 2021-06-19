eac3ae17c7e499f22d788e846e7db27d23cb5b0e
Fabian Garcia <fabian.garcia@almundo.com>
2021-06-19 13:27:42 -0500

    Added separated text recognition and other error case in tests, service enhance to resolve new error cases

--- src/api/iecho/service.js
+++ src/api/iecho/service.js
@@ -1,27 +1,40 @@
 const constants = require('./../../constants')
 
-const receivedTextIsValid = (text = '') => !!text
+const textValidation = (text = '') => !!text
+
+const textHasOnlySpaces = (text = '') => !!text &&
+  !text.split('')
+    .some(char => char !== ' ')
 
 const throwError = (errorText) => {
   throw errorText
 }
 
-const reverseText = (text) => {
+const reverseText = (text = '') => {
   const stringArray = text.split('')
   const reversedTextArray = stringArray.reverse()
   return reversedTextArray.join('')
 }
 
-const receivedTextIsPalindrome = (text, reversedText) => {
-  return text.toLowerCase() === reversedText.toLowerCase()
+const receivedTextIsPalindrome = (text = '', reversedText = '') => {
+  const textWithoutSpaces = text.split('')
+    .filter(char => char !== ' ')
+    .join('')
+  const reversedTextWithoutSpaces = reversedText.split('')
+    .filter(char => char !== ' ')
+    .join('')
+  const textAreSame = textWithoutSpaces.toLowerCase() === reversedTextWithoutSpaces.toLowerCase()
+  return textAreSame && textWithoutSpaces.length > 1
 }
 
 const iechoService = (text = '') => {
   return new Promise((resolve) => {
     try {
-      !receivedTextIsValid(text) && throwError(constants.errors.noTextReceived)
-      const reversedText = reverseText(text)
-      const isPalindome = receivedTextIsPalindrome(text, reversedText)
+      !textValidation(text) && throwError(constants.errors.noTextReceived)
+      const stringParsed = String(text)
+      textHasOnlySpaces(stringParsed) && throwError(constants.errors.noTextReceived)
+      const reversedText = reverseText(stringParsed)
+      const isPalindome = receivedTextIsPalindrome(stringParsed, reversedText)
       const response = { text: reversedText, palindrome: isPalindome }
       resolve({ status: 200, response })
     } catch (error) {
